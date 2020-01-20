b9468dcd64e86c460333ac40ef5ceac317f5ab0e
Lugh Wilson <lughjw@gmail.com>
2020-01-20 16:14:03 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,9 +1,16 @@
 # Code your solution here!
 def run_guessing_game
-  
+  number_to_guess = generate_number
+  guess = user_guess
+  if 
 end
 
-def generate_number(min=14, max=17)
+def generate_number(min=1, max=6)
   seed = max-min+1
   rand(seed)+min
+end
+
+def user_guess
+  puts "Please guess a number between 1 and 6"
+  gets
 end
